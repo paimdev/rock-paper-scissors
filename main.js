a911bc2f46cff85687e5aaf1d5adb1ea02e2cdb0
LucasBathke <bathkelucas@gmail.com>
2021-06-16 14:07:56 -0300

    round function algo added

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 // make a function called computerPlay
 function computerPlay() {
-//      get a random number from 0 to 9 and store it in a variable
+// get a random number from 0 to 9 and store it in a variable
     let randomNumber = Math.random() * 10;
-//      check if this random number is smaller or equal to 3
+// check if this random number is smaller or equal to 3
         if (randomNumber <= 3) {
-//          if yes return rock
+// if yes return rock
             return "rock";
         }
-//      check if this random number is bigger than 3 and smaller than 6
+// check if this random number is bigger than 3 and smaller than 6
         if (randomNumber >= 3 && randomNumber < 6) {
-//          if yes return paper
+// if yes return paper
             return "paper";
         }
-//      check if this random number is bigger than 6 and smaller than 9
+// check if this random number is bigger than 6 and smaller than 9
         if (randomNumber > 6 && randomNumber < 9)
-//          if yes return scissor
+// if yes return scissor
         return "scissor";
 }
 
-console.log(computerPlay())
+//  make a function that plays a round and have two paramethers (playerSelection and computerSelection)
+//  define the variable computerSelection to be the return of the function computerPlay
+//  define the variable playerSelection and asks for it's value to the user, makes the user input case insensitive
+//  use a function to check which one won
+//  return the result of the round in a string like (""You Lose! Paper beats Rock"")
